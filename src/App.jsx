cb3325a1cfa70ec7871ae61d950eb822141c6aee
CaiqueRNeves <caiquerabelo2015@hotmail.com>
2024-10-13 17:43:09 -0300

    Componente de Responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import QuemSomos from './componentes/QuemSomos';
 import CatalogoDeProdutos from './componentes/CatalogoDeProdutos';
 import Cop30NossoSite from './componentes/Cop30NossoSite';
 import Banner from './componentes/Banner';
+import styled, { keyframes } from 'styled-components';
 
 function App() {
   return (
@@ -16,6 +17,34 @@ function App() {
       <CatalogoDeProdutos/>
       <Banner /><br /><br />
       {/* Outros componentes ou conteúdo aqui */}
+    </div>
+  );
+}
+
+const bounce = keyframes`
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(-30px);
+  }
+  60% {
+    transform: translateY(-15px);
+  }
+`;
+
+const BouncingDiv = styled.div`
+  display: inline-block;
+  padding: 20px;
+  background-color: #61dafb;
+  border-radius: 5px;
+  animation: ${bounce} 2s infinite;
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <BouncingDiv>Salte!</BouncingDiv>
     </div>
   );
 }
